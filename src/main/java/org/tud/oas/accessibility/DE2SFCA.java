147f0e98da0d636c64c6dd0d583e031b77b604cb
ttpr0 <ttpro@gmx.de>
2023-08-06 23:14:57 +0200

    added dynamic fca methods

--- src/main/java/org/tud/oas/accessibility/DE2SFCA.java
+++ src/main/java/org/tud/oas/accessibility/DE2SFCA.java
@@ -5,22 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.tud.oas.accessibility.distance_decay.IDistanceDecay;
 import org.tud.oas.demand.IDemandView;
 import org.tud.oas.routing.IRoutingProvider;
 import org.tud.oas.routing.ITDMatrix;
 import org.tud.oas.routing.RoutingOptions;
 import org.tud.oas.supply.ISupplyView;
 
-public class Dynamic2SFCA {
+public class DE2SFCA {
 
-    public static float[] calc2SFCA(IDemandView demand, ISupplyView supply, List<Double> ranges, int[] rangeIndices,
-            IRoutingProvider provider) {
+    public static float[] calc2SFCA(IDemandView demand, ISupplyView supply, List<IDistanceDecay> decays,
+            int[] decayIndices, IRoutingProvider provider, String mode, List<Double> ranges) {
         float[] populationWeights = new float[demand.pointCount()];
         float[] facilityWeights = new float[supply.pointCount()];
 
-        Map<Integer, List<Integer>> invertedMapping = new HashMap<>();
+        Map<Integer, List<FacilityReference>> invertedMapping = new HashMap<>();
 
-        ITDMatrix matrix = provider.requestTDMatrix(demand, supply, "isochrones", new RoutingOptions(ranges));
+        ITDMatrix matrix = provider.requestTDMatrix(demand, supply, mode, new RoutingOptions(ranges));
         try {
             if (matrix == null) {
                 return populationWeights;
@@ -32,16 +33,15 @@ public class Dynamic2SFCA {
                     if (range == 9999) {
                         continue;
                     }
-                    if (range > ranges.get(rangeIndices[p])) {
-                        continue;
-                    }
+                    IDistanceDecay decay = decays.get(decayIndices[p]);
+                    float rangeFactor = decay.getDistanceWeight(range);
                     int populationCount = demand.getDemand(p);
-                    weight += populationCount;
+                    weight += populationCount * rangeFactor;
 
                     if (!invertedMapping.containsKey(p)) {
                         invertedMapping.put(p, new ArrayList<>(4));
                     }
-                    invertedMapping.get(p).add(f);
+                    invertedMapping.get(p).add(new FacilityReference(f, range));
                 }
                 if (weight == 0) {
                     facilityWeights[f] = 0;
@@ -51,13 +51,15 @@ public class Dynamic2SFCA {
             }
 
             for (int index : invertedMapping.keySet()) {
-                List<Integer> refs = invertedMapping.get(index);
+                List<FacilityReference> refs = invertedMapping.get(index);
                 if (refs == null) {
                     continue;
                 } else {
                     float weight = 0;
-                    for (int fref : refs) {
-                        weight += facilityWeights[fref];
+                    for (FacilityReference fref : refs) {
+                        IDistanceDecay decay = decays.get(decayIndices[index]);
+                        double rangeFactor = decay.getDistanceWeight(fref.range);
+                        weight += facilityWeights[fref.index] * rangeFactor;
                     }
                     populationWeights[index] = weight;
                 }
